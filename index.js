02d1a6ec862a0dad0198fa660b0c4017628bf784
Aryanbhalla01 <aryanbhalla040@gmail.com>
2026-02-11 21:10:01 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+// NAME :- ARYAN BHALLA
+// URN :- 2581204
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("IRIS Safety Server is Running");
+});
+
+app.post("/emergency", (req, res) => {
+  const { name, location } = req.body;
+
+  console.log("EMERGENCY ALERT");
+  console.log("User:", name);
+  console.log("Location:", location);
+
+  res.send("Emergency alert sent!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
